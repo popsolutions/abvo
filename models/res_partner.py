7a43f5057cbe6f9676efd0d7c787449f31b16036
Jaqueline Passos do Nascimento <jaqueline.passos@popsolutions.co>
2020-09-04 10:11:02 -0300

    Added boat type

--- models/res_partner.py
+++ models/res_partner.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from odoo import api, fields, models
 from . import abvo_certificates
 
@@ -6,8 +8,8 @@ class ResPartner(models.Model):
     _name = 'res.partner'
     _inherit = 'res.partner'
 
-    # boat_partner_id = fields.Many2one('res.partner') 
-    # company_type = 
+    company_type = fields.Selection(selection_add=[('boat','Embarcação')], 
+                                    compute='_compute_company_type', inverse='_write_company_type')
 
     estaleiro = fields.Char(u'Estaleiro')
     modelo = fields.Char(u'Modelo')
@@ -19,9 +21,29 @@ class ResPartner(models.Model):
     certificates_lines = fields.One2many('abvo.certificates', 'partner_id',
                                          readonly=True, copy=True)
 
-    # lines_ids = fields.One2many('pops.measurement.lines', 'measurement_id', 'Measurement Lines', readonly=True,
-    #                             copy=True)
+    is_boat = fields.Boolean(default=False)
 
-    # photo_lines_ids = fields.One2many('pops.measurement.photolines', 'measurement_id', 'Measurement Photo Lines',
-                                      # readonly=True, copy=True)
-    # missions_id = fields.Many2one('pops.missions', 'Missions', ondelete='cascade', required=True)
+    def _write_company_type(self):
+        for partner in self:
+            if partner.company_type == 'boat':
+                self.is_boat = True
+                self.is_company = True
+            else:
+                partner.is_company = partner.company_type == 'company'
+
+    @api.depends('is_company')
+    def _compute_company_type(self):
+        for partner in self:
+            if partner.is_boat == True:
+                partner.company_type = 'boat'
+            else:
+                partner.company_type = 'company' if partner.is_company else 'person'
+
+    @api.onchange('company_type')
+    def onchange_company_type(self):
+        for partner in self:
+            if self.company_type == 'boat':
+                self.is_boat = True
+                self.is_company = True
+            else: 
+                self.is_company = (self.company_type == 'company')
