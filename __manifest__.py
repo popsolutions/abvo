957dadd9d070713935e91207521bf2ada09f6a69
Jaqueline Passos do Nascimento <jaqueline.passos@popsolutions.co>
2020-09-03 23:25:18 -0300

    Add icon

--- __manifest__.py
+++ __manifest__.py
@@ -3,15 +3,14 @@
     'name': "abvo",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Add Certificates to ABVO Website""",
 
     'description': """
-        Long description of module's purpose
+        Add Certificates to ABVO Website
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "POPSOLUTIONS.CO",
+    'website': "https://www.popsolutions.co",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
