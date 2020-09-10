c2fc18d5410d418a287e48b87f4bc6f8327696c1
Marcos <mendez.foto@gmail.com>
2020-09-10 17:54:30 -0300

    Updated Dependes

--- __manifest__.py
+++ __manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'portal', 'contacts'],
+    'depends': ['base', 'portal', 'contacts', 'membership'],
 
     # always loaded
     'data': [
