8e8e71a39a70ca321b8f135f46034d2013bde0b4
Marcos Mendez <mendez.foto@gmail.com>
2020-09-23 03:27:05 +0000

    Updates Module Version

--- __manifest__.py
+++ __manifest__.py
@@ -16,7 +16,7 @@
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '0.9.23',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'portal', 'contacts', 'membership'],
